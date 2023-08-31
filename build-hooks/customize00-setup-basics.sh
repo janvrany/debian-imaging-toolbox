8e6651ceaf224bb657b26011b0552feaff234f04
Jan Vrany <jan@vrany.io>
2023-08-31 08:46:09 +0100

    Install nfs-common to allow mounting NFS volumes

--- build-hooks/customize00-setup-basics.sh
+++ build-hooks/customize00-setup-basics.sh
@@ -47,7 +47,8 @@ chroot "${ROOT}" /usr/bin/apt-get --allow-unauthenticated -y install \
     apt-utils openssh-client \
     sudo bash-completion tmux adduser acl ethtool \
     procps udev locales zip unzip \
-    lsb-release dbus man bsd-mailx
+    lsb-release dbus man bsd-mailx \
+    nfs-common
     # libgnutls30 \
 
 #
