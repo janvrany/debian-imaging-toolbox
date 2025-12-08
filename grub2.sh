c9d4d1691a28039205a492e1463866510693daaf
Jan Vrany <jan@vrany.io>
2025-12-08 14:09:26 +0000

    Update `grub2.sh` to support bare metal images
    This commit updates `grub2.sh` to allow installing GRUB into
    images that are intended to run wide range of real hardware.
    
    Prior this commit, `grub2.sh`-installed boot was optimized for
    QEMU/KVM virtual machines (serial console, minimal initramfs
    and so on).
    
    With this commit by using new option `-g` the boot process is
    made a generic as possible, allowing one to use `dd` to install
    image onto real hardware and boot it.

--- grub2.sh
+++ grub2.sh
@@ -2,6 +2,11 @@
 #
 # Install Linux kernel and GRUB2 into disk image.
 #
+# This script currently supports only PCs with (legacy) BIOS.
+# Other architectures and/or UEFI is not supported yet.
+#
+# 
+#
 source "$(dirname $(realpath ${BASH_SOURCE[0]}))/functions.sh"
 config "$(dirname $0)/config.sh" || error "Cannot read config.sh: $1"
 config "$(dirname $0)/config-local.sh"
@@ -14,13 +19,47 @@ config "$(dirname $0)/config-local.sh"
 
 
 #
+# Options
+#
+usage() {
+	echo "Install GRUB bootloader given the disk image.
+
+Usage: $0 [-h] [-g] IMAGE
+
+  -g ... Use to make image bootable on wide range of hardware and virtual machines.
+         Without -g, image is optimized to boot quickly on KVM/QEMU virtual machines.
+
+  -h ... Print this message.
+" 1>&2;
+}
+
+optimize_for_kvm_vm=yes
+
+while getopts ":gh" o; do
+    case "${o}" in
+        g)
+            optimize_for_kvm_vm=no
+            ;;
+        h)
+            usage
+            exit 0
+            ;;
+        *)
+            error "Unknown option: ${OPTARG}"
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+#
 # Install Linux kernel and GRUB2
 #
 if [ -z "$1" ]; then
-    echo "usage: $(basename $0) <ROOT>"
+    usage
     exit 1
 elif [ -d "$1" ]; then
 	error "Invalid root filesystem image (directory): $1"
+	usage
 	exit 2
 fi
 
@@ -43,7 +82,11 @@ fi
 
 
 sudo chroot "${ROOT}" apt-get --allow-unauthenticated -y install \
-						"$kernel_pkg" initramfs-tools grub-pc
+						"$kernel_pkg" initramfs-tools zstd grub-pc
+if [ "$optimize_for_kvm_vm" == "yes" ]; then
+	sudo chroot "${ROOT}" apt-get --allow-unauthenticated -y install \
+		qemu-guest-agent
+fi
 sudo chroot "${ROOT}" apt-get clean
 
 echo "
@@ -57,6 +100,45 @@ int main(int argc, char *argv[]) {
 " > "$ROOT/tmp/off.c"
 gcc -static -o "$ROOT/tmp/off" "$ROOT/tmp/off.c"
 
+
+echo '
+#
+# Disable os prober. There are no other systems.
+#
+GRUB_DISABLE_OS_PROBER=true
+' | sudo tee "$ROOT/etc/default/grub.d/os-prober.cfg"
+
+echo '
+#
+# Make root filesystem writable. Why the hell is
+# this needed?
+#
+GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX rw"
+' | sudo tee "$ROOT/etc/default/grub.d/rw.cfg"
+
+echo '
+#
+# Always include virtio_blk module in initramfs
+#
+virtio_blk"
+' | sudo tee -a "$ROOT/etc/initramfs-tools/modules"
+
+if [ ! -z "$CONFIG_GRUB_CMDLINE_LINUX_CUSTOM" ]; then
+echo "
+#
+# Custom linux cmdline options
+#
+GRUB_CMDLINE_LINUX=\"\$GRUB_CMDLINE_LINUX $CONFIG_GRUB_CMDLINE_LINUX_CUSTOM\"
+" | sudo tee "$ROOT/etc/default/grub.d/custom.cfg"
+fi
+
+#
+# Following options are used only when the image is intedent
+# to run on KVM/QEMU VMs only.
+#
+
+if [ "$optimize_for_kvm_vm" == "yes" ]; then
+
 echo '
 #
 # Enable serial console in Linux:
@@ -68,14 +150,6 @@ GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX console=ttyS0"
 #
 GRUB_TERMINAL=console
 ' | sudo tee "$ROOT/etc/default/grub.d/console.cfg"
-
-echo '
-#
-# Make root filesystem writable. Why the hell is
-# this needed?
-#
-GRUB_CMDLINE_LINUX="$GRUB_CMDLINE_LINUX rw"
-' | sudo tee "$ROOT/etc/default/grub.d/rw.cfg"
 
 echo '
 #
@@ -99,13 +173,6 @@ GRUB_TIMEOUT=1
 
 echo '
 #
-# Disable os prober. There are no other systems.
-#
-GRUB_DISABLE_OS_PROBER=true
-' | sudo tee "$ROOT/etc/default/grub.d/os-prober.cfg"
-
-echo '
-#
 # Only include required modules in initramfs. This significantly
 # reduces the size of initramfs and speeds up boot. This is especially
 # handy when used as start-on-demand CI build node.
@@ -113,20 +180,13 @@ echo '
 MODULES=dep
 ' | sudo tee "$ROOT/etc/initramfs-tools/conf.d/modules"
 
-if [ ! -z "$CONFIG_GRUB_CMDLINE_LINUX_CUSTOM" ]; then
-echo "
-#
-# Custom linux cmdline options
-#
-GRUB_CMDLINE_LINUX=\"\$GRUB_CMDLINE_LINUX $CONFIG_GRUB_CMDLINE_LINUX_CUSTOM\"
-" | sudo tee "$ROOT/etc/default/grub.d/custom.cfg"
-fi
+fi # if [ "$optimize_for_kvm_vm" == "yes" ]...
 
 echo "#!/bin/bash
 set -x
 update-grub2
 update-initramfs -c -k all
-grub-install /dev/vda
+grub-install $(if [ "${optimize_for_kvm_vm}" == "yes" ]; then echo /dev/vda; else echo /dev/sda; fi)
 /tmp/off
 " | sudo tee "$ROOT/tmp/grub-self-install.sh"
 sudo chmod ugo+x "$ROOT/tmp/grub-self-install.sh"
@@ -164,9 +224,25 @@ rm -f $tmp/vmlinuz $tmp/initrd.img
 mv $tmp/$(basename $vmlinuz) $tmp/vmlinuz
 mv $tmp/$(basename $initrd) $tmp/initrd.img
 
-qemu-system-x86_64 \
-    -M q35 -m "512M" \
-	-kernel "$tmp/vmlinuz" -initrd "$tmp/initrd.img" -append "root=/dev/vda1 rw console=ttyS0 init=/tmp/grub-self-install.sh" \
-	-nographic \
-	-drive if=none,id=drive0,cache=none,aio=native,file=$1,format=$root_img_fmt -device virtio-blk-pci,drive=drive0 \
-	-netdev user,id=hostnet0 -device virtio-net-pci,netdev=hostnet0
+if [ "$optimize_for_kvm_vm" == "yes" ]; then
+	# If the image is intended to boot only on KVM/QEMU, then use virtio
+	# block device (which will likely be used). Otherwise, update-initramfs
+	# would pull in SATA modules, making initrd larger and this slowing down
+	# boot.
+
+	# TODO: Fix hardcoded /dev/vda1!
+	qemu-system-x86_64 \
+    	-M q35 -m "512M" \
+		-kernel "$tmp/vmlinuz" -initrd "$tmp/initrd.img" -append "root=/dev/vda1 rw console=ttyS0 init=/tmp/grub-self-install.sh" \
+		-nographic \
+		-drive "file=$1,format=$root_img_fmt,if=virtio" \
+		-netdev user,id=hostnet0 -device virtio-net-pci,netdev=hostnet0
+else
+	# Otherwise, use SATA. Note that virtio_blk module us always included so
+	# this image should boot using virtio block device too,
+	qemu-system-x86_64 \
+    	-M q35 -m "512M" \
+		-kernel "$tmp/vmlinuz" -initrd "$tmp/initrd.img" -append "root=$root_dev rw init=/tmp/grub-self-install.sh" \
+		-drive if=none,id=disk0,cache=none,aio=native,file=$1,format=$root_img_fmt -device ahci,id=ahci -device ide-hd,drive=disk0,bus=ahci.0 \
+		-netdev user,id=hostnet0 -device virtio-net-pci,netdev=hostnet0
+fi
